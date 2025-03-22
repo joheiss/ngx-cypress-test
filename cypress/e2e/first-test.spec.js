eb1c7cbe96a92c6184e06ec84f19ee677490a5e0
Josef Heiss <josef.heiss@jovisco.de>
2025-03-22 19:56:11 +0100

    after lesson 19

--- cypress/e2e/first-test.spec.js
+++ cypress/e2e/first-test.spec.js
@@ -1,7 +1,23 @@
+/// <reference types="cypress" />
+
 describe("first test", () => {
 
-  it("should run as expected", () => {
+  beforeEach(() => {
     cy.visit("/");
   });
 
+  it("should contain 'ngx-admin' in header", () => {
+    cy.get("div.header-container div.logo-container").within(
+      () => cy.get("a.logo").contains("ngx-admin").should("exist")
+    );
+  });
+
+  it("should expand the 'Layout' menu, when clicked", () => {
+    cy.get("ul.menu-items li a").contains("Layout").as("layout-menu-item");
+    cy.get("@layout-menu-item").click();
+    // cy.get("@layout-menu-item").parent().within(() => {
+    //   cy.get("ul.menu-items li.menu-item")
+    //     .should("have.length", 2);
+    // });
+  });
 });
